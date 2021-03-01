0a9322652196fe25e7a78f34a7d48a3336cd397f
WXY <479525390@qq.com>
2021-03-01 19:06:42 +0800

    根据接口文档定义接口请求

--- src/api/ajax.jsx
+++ src/api/ajax.jsx
@@ -5,8 +5,15 @@ import axios from 'axios'
 export default function ajax(url,data={},type="GET") {
     if(type==='GET'){
         //发get请求
-        return axios.get()
+        return axios.get(url,{//配置对象
+            params:data // 指定请求参数
+        })
     }else{//发post请求
-        return axios.post()
+        return axios.post(url,{
+            data
+        })
     }
 }
+
+// ajax('/login',{username:'tom',password:'12345'},'post').then()
+// ajax('/manage/user/add',{username:'tom',password:'12345',phone:'12324525525252'},'post').then()
