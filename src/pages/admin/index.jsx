ca75cb73d336b09989c68f6ec09b3231b6296914
WXY <479525390@qq.com>
2021-03-01 22:39:27 +0800

    歇逼

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
-import memoryUtils from '../../utils/memoryUtils'
-import {Redirect} from 'react-router-dom'
-export default class Admin extends Component {
+import React, { Component } from "react";
+import memoryUtils from "../../utils/memoryUtils";
+import { Redirect } from "react-router-dom";
+import { Layout } from "antd";
 
-    render() {
-        const user = memoryUtils.user
-        // 如果内存中没有存储user ==>当前没登陆
-        if(!user||!user._id){
-            //自动跳转到登陆
-            return <Redirect to='/login'/>
-        }
-        return (
-            <div>
-                Hello,{user.username}
-            </div>
-        )
+const { Header, Footer, Sider, Content } = Layout;
+
+export default class Admin extends Component {
+  render() {
+    const user = memoryUtils.user;
+    // 如果内存中没有存储user ==>当前没登陆
+    if (!user || !user._id) {
+      //自动跳转到登陆
+      return <Redirect to="/login" />;
     }
+    return (
+      <>
+        <Layout style={{height:'100%'}}>
+          <Sider>Sider</Sider>
+          <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+          </Layout>
+        </Layout>
+      </>
+    );
+  }
 }
