b66d1d4dbf82e1b956e27bfd7487378ab4985249
WXY <479525390@qq.com>
2021-03-01 17:02:29 +0800

    添加.gitignore

--- src/api/ajax.jsx
+++ src/api/ajax.jsx
@@ -1,2 +1,12 @@
 // 能发送异步ajax请求的函数模块
 // 封装axios
+import axios from 'axios'
+
+export default function ajax(url,data={},type="GET") {
+    if(type==='GET'){
+        //发get请求
+        return axios.get()
+    }else{//发post请求
+        return axios.post()
+    }
+}
