f4ee3535e7cf1f0b2ab2e8dc567d3ddce3560910
WXY <479525390@qq.com>
2021-03-03 20:54:12 +0800

    添加高德api

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,32 @@
-import ajax from './ajax.jsx'
+// import json from jsonp
+import {message} from 'antd'
+import ajax from "./ajax.jsx";
+import jsonp from "jsonp";
 // 包含应用中所有接口请求函数的模块
 // 要求:能根据接口文档定义接口请求
 // 每个函数的返回值都是promise
 
 //登陆
 
-export  const reqLogin = (username,password)=> ajax('/login',{username,password},'POST')
+export const reqLogin = (username, password) =>
+  ajax("/login", { username, password }, "POST");
 
-export  const reqAddUser=(user)=>  ajax('/manage/user/add',user,'POST')
+export const reqAddUser = (user) => ajax("/manage/user/add", user, "POST");
+
+/* jsonp请求的接口请求函数 */
+export const reqWeather = (city) => {
+  return new Promise((resolve, reject) => {
+    const url = `http://restapi.amap.com/v3/weather/weatherInfo?key=98c97d10c1fda37bdc5402d15c1cdd71&city=${city}`;
+    jsonp(url, {}, (err, data) => {
+    //   console.log(err, data);
+      if (!err && data.status === "1") {
+        const { temperature, weather } = data.lives[0];
+        resolve({ weather});
+      }else{
+          message.error('天气获取失败')
+      }
+    });
+  });
+};
+
+// reqWeather("北京");
--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -1,17 +1,52 @@
 import React, { Component } from "react";
-import { PageHeader } from "antd";
-
+// import { PageHeader } from "antd";
+import "./index.less";
+import {reqWeather} from '../../api/index'
+import { CloudOutlined } from "@ant-design/icons";
+import {formateDate} from '../../utils/dataUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import { set } from "store";
 export default class Header extends Component {
+  state= {
+    currentTime:formateDate(Date.now()),
+    weather:'',
+  }
+  getTime=() =>{
+
+    setInterval(()=>{
+       this.setState({currentTime:formateDate(Date.now())}) 
+    },1000)
+  }
+  getWeather=async ()=>{
+    // console.log(reqWeather('杭州'))
+    const {weather}=await reqWeather('杭州')
+    // console.log(weather)
+    this.setState({weather})
+  }
+  /* 第一次render后立即执行 */
+  componentDidMount(){
+    this.getTime()
+    this.getWeather()
+  }
   render() {
+    const {currentTime,weather} = this.state
+    const user =  memoryUtils.user.username
     return (
-      <div>
-        <PageHeader
-          className="site-page-header"
-          onBack={() => null}
-          title="Title"
-          subTitle="This is a subtitle"
-        />
-        ,
+      <div className="header">
+        <div className="header-top">
+          <span>欢迎，{user} </span>
+          <a href="javascript:">退出</a>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">
+            <span>首页</span>
+          </div>
+          <div className="header-bottom-right">
+          <span>{currentTime} </span>
+              <CloudOutlined  style={{width:'30px',height:'20px', margin: "15 15 15 15" }}/>
+            <span>{weather} </span>
+          </div>
+        </div>
       </div>
     );
   }
